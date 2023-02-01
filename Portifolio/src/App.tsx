e63d197a977f816d36882c7238d487a9b04e2ab3
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-01 00:32:25 -0300

    Update

--- Portifolio/src/App.tsx
+++ Portifolio/src/App.tsx
@@ -1,14 +1,23 @@
-import { zinc } from 'tailwindcss/colors'
 
-function App() {
+import './styles/global.css'
+import image from "./img/background.jpg"
+
+export function App() {
 
   return (
-    <div style={{backgroundColor: "lightblue"}}>
-      <h1 style={{color: "red"}}>
-        Hello World!
-      </h1>
+    <div className='w-screen h-screen  flex-col bg-zinc-900 items-center justify-center gap-4'>
+      <div className="bg-fixed" style={{ backgroundImage: `url(${image})` }}></div>
+
+
+
+      <p className='text-5xl text-white'>I'm a Web</p>
+  
+      <p className='text-5xl antialiased text-blue-400 hover:text-sky-900'>Developer</p>
+      <div className="w-200 h-200 bg-zinc-500"></div>
+      <svg className='animate-bounce w-20 h-20 fill-blue-500 hover:fill-white' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+      </svg>
+
     </div>
   )
 }
-
-export default App
