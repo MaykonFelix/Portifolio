6ba80f60cd4d5296e80d2d5d3702fab6b4288d96
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-16 14:00:54 -0300

    Update

--- Portifolio/src/App.tsx
+++ Portifolio/src/App.tsx
@@ -1,23 +1,10 @@
 
-import './styles/global.css'
-import image from "./img/background.jpg"
+import Index from './Pages/Home/Index'
 
 export function App() {
 
   return (
-    <div className='w-screen h-screen  flex bg-zinc-900 items-center justify-center gap-4'>
-      <div className="bg-fixed" style={{ backgroundImage: `url(${image})` }}></div>
+    <Index />
 
-
-
-      <p className='text-5xl text-white'>I'm a Web</p>
-  
-      <p className='text-5xl antialiased text-blue-400 hover:text-sky-900'>Developer</p>
-      <div className="w-200 h-200 bg-zinc-500"></div>
-      <svg className='animate-bounce w-20 h-20 fill-blue-500 hover:fill-white' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-      </svg>
-
-    </div>
   )
 }
---
+++ Portifolio/src/Pages/Home/Index.tsx
@@ -0,0 +1,58 @@
+
+import "./style.css";
+
+import MySVG from '../../assets/logo1.svg'
+
+
+export default function Index() {
+    return (
+        <div id="wrapper">
+            <header>
+                <h1 className="left">
+                    <img src={MySVG} alt="Logo" />
+                </h1>
+
+                <nav className="middle">
+                    <ul>
+                        <li><a href="#">Solution</a></li>
+                        <li><a href="#">Problems</a></li>
+                        <li><a href="#">Price</a></li>
+                    </ul>
+                </nav>
+
+                <div className="right">
+                    <a className="conf">
+                        <img src="assets/special-button.svg" alt="Special Conference" />
+                        <span>WoW !!!</span>
+                    </a>
+
+                    {/* https://getavataaars.com/ */}
+                    <img className="avatar" src="assets/avatar.svg" alt="Avatar" />
+                </div>
+            </header>
+
+            <main>
+
+                <h1>
+                    <span>Developer.</span>
+                    <span>Preview.</span>
+                    <span>Ship.</span>
+                </h1>
+
+                <div className="buttons">
+                    <button>Coding</button>
+                    <button className="outlined">Get a Demo</button>
+                </div>
+
+                <h2>Project to learn about animation of gradients. A very beautiful thing to see.</h2>
+
+
+            </main>
+
+            <footer>Explore the Vercel way</footer>
+        </div>
+    )
+}
+
+
+
