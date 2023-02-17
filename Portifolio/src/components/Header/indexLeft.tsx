fbb425ff5ba0d3cc22cf5ae50b72e3ebdddfa5cc
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-17 13:38:36 -0300

    Update

---
+++ Portifolio/src/components/Header/indexLeft.tsx
@@ -0,0 +1,19 @@
+import { IoMdImage } from 'react-icons/io'
+
+export default function indexLeft() {
+    return (
+        <div className='header'>
+            <nav>
+                <ul className="ul-tems">
+                    <li>
+                        <IoMdImage />
+                    </li>
+
+                </ul>
+
+            </nav>
+
+
+        </div>
+    )
+}
