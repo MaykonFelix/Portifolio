a81730c55c255a1444a438522da90699a3b7e6c3
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-18 01:12:46 -0300

    Update

--- Portifolio/src/App.tsx
+++ Portifolio/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react"
-import Features from "./component/Features/Features"
 import Portfolio from "./component/Portfolio/Portfolio"
 import Resume from "./component/Resume/Resume"
 import Testimonial from "./component/Testimonial/Testimonial"
 import Blog from "./component/Blog/Blog"
 import Contact from "./component/Contact/Contact"
-
-
-
+import Features from "./component/Features/Features"
 import Header from "./component/head"
 import Home from "./component/Home"
 import Footer from "./component/footer"
