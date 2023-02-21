792a4549d11775d9f949f6c29a890964ac072c36
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-21 17:00:18 -0300

    UPdate

--- Portifolio/src/component/Features/Features-Api.tsx
+++ Portifolio/src/component/Features/Features-Api.tsx
@@ -1,49 +1,29 @@
 const data = [
   {
     id: 1,
-    image: "https://img.icons8.com/ios-filled/50/null/combo-chart--v2.png",
+    image: "https://img.icons8.com/color/48/null/combo-chart--v1.png",
     title: "Crescimento Empresarial",
     desc: " Contribuo com o crescimento empresarial com medidas inovadoras e conhecimentos práticos, acredito que todo serviço me ajuda ampliar meus conhecimentos profissionais e de somar esforços juntos com a empresa que estou atuando.",
   },
 
   {
-    id: 7,
-    image: "https://img.icons8.com/ios/64/000000/menu.png",
-    title: "Business Stratagy",
-    desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
-  },
-  {
     id: 2,
-    image: "https://img.icons8.com/ios/40/000000/open-book.png",
-    title: "App Development",
-    desc: " It uses a dictionary of over 200 Latin words, combined witha handful of model sentence.",
+    image: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-web-development-web-development-flaticons-lineal-color-flat-icons.png",
+    title: "Web Developer",
+    desc: "Contribuindo atualmente com o desenvolvimento Web em tecnologias inovadoras para atender as necessidades da empresa que estou atuando, utilizando ReactJS, Node e MYSQL. ",
   },
   {
     id: 3,
-    image: "https://img.icons8.com/fluency-systems-regular/64/000000/retro-tv.png ",
-    title: "App Development",
-    desc: "I throw myself down among the tall grass by the stream as I lie close to the earth.",
+    image: "https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/64/null/external-agile-web-design-and-development-flatart-icons-lineal-color-flatarticons.png",
+    title: "Método Ágil",
+    desc: "Contribui na atual empresa a implementação da metodologia ágil 'Metodo Kamblam'.",
   },
   {
     id: 4,
-    image: "https://img.icons8.com/material-outlined/64/000000/topic--v1.png ",
-    title: "Mobile App",
-    desc: "There are many variations of passages of Lorem Ipsum	available, but the majority.",
+    image: "https://img.icons8.com/fluency/48/null/design.png",
+    title: "Design Grafico",
+    desc: "Contribui na atual empresa a implementação da metodologia ágil 'Metodo Kamblam'.",
   },
-  {
-    id: 5,
-    image: "https://img.icons8.com/ios-filled/64/000000/wifi.png ",
-    title: "CEO Marketing",
-    desc: "always free from repetition, injected humour, or non-characteristic words etc.",
-  },
-  {
-    id: 6,
-    image: "https://img.icons8.com/glyph-neue/50/000000/polyline.png ",
-    title: "Personal Portfolio April",
-    desc: " It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.",
-  },
-
-
 
 ]
 export default data
