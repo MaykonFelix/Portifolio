ad488d10d80c7c944029e36108526a54d1829133
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-23 14:34:44 -0300

    Update

--- Portifolio/src/component/Portfolio/Card.tsx
+++ Portifolio/src/component/Portfolio/Card.tsx
@@ -6,6 +6,7 @@ interface propsModal {
   totalLike: string;
   title: string;
   description?: string;
+  webLink: string;
 }
 
 const Card = (props: propsModal) => {
@@ -23,28 +24,35 @@ const Card = (props: propsModal) => {
   return (
     <>
       <div className='box btn_shadow '>
+
         <div className='img'>
           <img src={props.image} alt='Construction' onClick={toggleModal} />
         </div>
+
         <div className='category d_flex'>
           <span onClick={toggleModal}>{props.category}</span>
           <label>
             <i className='far fa-heart'></i> {props.totalLike}
           </label>
         </div>
+
         <div className='title'>
           <h2 onClick={toggleModal}>{props.title}</h2>
           <a href='#popup' className='arrow' onClick={toggleModal}>
             <i className='fas fa-arrow-right'></i>
           </a>
         </div>
+
       </div>
 
       {/* Popup box */}
       {modal && (
         <div className='modal'>
+
           <div onClick={toggleModal} className='overlay'></div>
+
           <div className='modal-content d_flex'>
+
             <div className='modal-img left'>
               <img src={props.image} alt='' />
             </div>
@@ -54,12 +62,19 @@ const Card = (props: propsModal) => {
               <h1>{props.title}</h1>
               <p>{props.description}</p>
               <div className='button f_flex mtop'>
+
                 <button className='btn_shadow'>
                   LIKE THIS <i className='far fa-thumbs-up'></i>
                 </button>
-                <button className='btn_shadow'>
-                  VIEW PROJECT<i className='fas fa-chevron-right'></i>
-                </button>
+
+                <a href={props.webLink}>
+
+                  <button className='btn_shadow'>
+                    VIEW PROJECT<i className='fas fa-chevron-right'></i>
+                  </button>
+                </a>
+
+
               </div>
               <button className='close-modal btn_shadow' onClick={toggleModal}>
                 <i className='fas fa-times'></i>
--- Portifolio/src/component/Portfolio/Portfolio_data.tsx
+++ Portifolio/src/component/Portfolio/Portfolio_data.tsx
@@ -1,7 +1,9 @@
 
 import Imagem01 from "./image/portfolio-01.jpg"
 import Imagem02 from "./image/portfolio-02.jpg"
-import Imagem03 from "./image/portfolio-03.jpg"
+import Imagem03 from "./image/portfolio-03.png"
+import Imagem04 from "./image/portfolio-04.jpg"
+
 
 const Portfolio_data = [
   {
@@ -11,6 +13,25 @@ const Portfolio_data = [
     title: "Serviço de Web Developer ",
     description: "Serviço Realizado ",
     image: Imagem01,
+    webLink: "https://www.figma.com/file/k6oiH7T7IZCVFl4QSWViTQ/Origini-Italiane?node-id=112%3A8&t=cOokpkyN52vF8pNR-1",
+  },
+  {
+    id: 3,
+    category: "WEB DEVELOPER",
+    totalLike: "360",
+    title: "SITE ORIGINI ITALIANE",
+    description: "Serviço Realizado ",
+    image: Imagem04,
+    webLink: "aaa",
+  },
+  {
+    id: 4,
+    category: "FIGMA",
+    totalLike: "425",
+    title: "Modelagem Novo Site Origini Italiane SAS",
+    description: "Serviço realizado inicialmente para apresentar como o novo site vai ficar, assim os sócios poderem aprovar. E também para orientar na hora da implementação como espaçamentos, layout e design do CSS.",
+    image: Imagem03,
+    webLink: "aaa",
   },
   {
     id: 2,
@@ -19,14 +40,7 @@ const Portfolio_data = [
     title: "Logo design creativity & Application ",
     description: "Serviço Realizado ",
     image: Imagem02,
-  },
-  {
-    id: 3,
-    category: "FIGMA",
-    totalLike: "360",
-    title: "Mobile app landing design & Services",
-    description: "Serviço Realizado ",
-    image: Imagem03,
+    webLink: "aaa",
   },
 ]
 export default Portfolio_data
