4884f0edd3b6ddbf89c221a1b5cff1da8a3590c4
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-24 12:59:37 -0300

    Add Particle
    Add particle react...

--- Portifolio/src/App.tsx
+++ Portifolio/src/App.tsx
@@ -4,12 +4,15 @@ import Features from "./component/Features/Features"
 import Header from "./component/head"
 import Home from "./component/Home"
 import Footer from "./component/footer"
+import Partic from "./component/Particle"
 
 import "./global.css"
 
 const App = () => {
   return (
     <>
+
+      <Partic />
       <Header />
       <Home />
       <Features />
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://maykon.vercel.app/
 
 Sendo Construido todo dia
 Um tijolinho por dia
+
+# Particle
+https://particles.js.org/
