3714f496c480331deea14dcfab3d53f592e920f7
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-07-24 23:52:24 -0300

    improv menu lateral

--- Next-Portifolio/app/shared/components/MenuItems/index.tsx
+++ Next-Portifolio/app/shared/components/MenuItems/index.tsx
@@ -1,92 +1,79 @@
-import styles from "./styles.module.css";
+import styles from './styles.module.css'
 
-import SchoolIcon from "@mui/icons-material/School";
-import AutoStoriesIcon from "@mui/icons-material/AutoStories";
-import WorkIcon from "@mui/icons-material/Work";
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import SchoolIcon from '@mui/icons-material/School'
+import AutoStoriesIcon from '@mui/icons-material/AutoStories'
+import WorkIcon from '@mui/icons-material/Work'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown'
 
-import Zoom from "@mui/material/Zoom";
-import Tooltip from "@mui/material/Tooltip";
+import Zoom from '@mui/material/Zoom'
+import Tooltip from '@mui/material/Tooltip'
 
 export const MenuItems = () => {
-  return (
-    <section className={styles.MenuItens}>
-      <ul>
-        <li>
-          <span className={styles.icons}>
-            <Tooltip
-              title="Conhecimentos"
-              placement="left"
-              TransitionComponent={Zoom}
-              arrow
-            >
-              <a href="#features">
-                <SchoolIcon color="primary" />
-              </a>
-            </Tooltip>
-          </span>
-          <span>
-            <a href="#features" className={styles.text}>
-              Conhecimentos
-            </a>
-          </span>
-        </li>
-        <li>
-          <span>
-            <Tooltip
-              title="Portfólio"
-              placement="left"
-              TransitionComponent={Zoom}
-              arrow
-            >
-              <a href="#portfolio" className={styles.icons}>
-                <AutoStoriesIcon color="primary" />
-              </a>
-            </Tooltip>
-          </span>
-          <span>
-            <a href="#portfolio" className={styles.text}>
-              Portfólio
-            </a>
-          </span>
-        </li>
-        <li>
-          <span>
-            <Tooltip
-              title="Resumo"
-              placement="left"
-              TransitionComponent={Zoom}
-              arrow
-            >
-              <a href="#resume" className={styles.icons}>
-                <WorkIcon color="primary" />
-              </a>
-            </Tooltip>
-          </span>
-          <span>
-            <a href="#resume" className={styles.text}>
-              Resumo
-            </a>
-          </span>
-        </li>
-        <li>
-          <Tooltip
-            title="Subir"
-            placement="left"
-            TransitionComponent={Zoom}
-            arrow
-          >
-            <a href="#" target="_top" className={styles.icons}>
-              <ArrowCircleUpIcon color="primary" />
-            </a>
-          </Tooltip>
-          <span>
-            <a href="#" target="_top" className={styles.text}>
-              Subir
-            </a>
-          </span>
-        </li>
-      </ul>
-    </section>
-  );
-};
+	return (
+		<section className="flex flex-col justify-center items-center fixed z-10 top-1/3 right-6">
+			<ul>
+				<li className="text-center m-2">
+					<Tooltip
+						title="Conhecimentos"
+						placement="left"
+						TransitionComponent={Zoom}
+						arrow
+					>
+						<a href="#features">
+							<SchoolIcon color="primary" />
+						</a>
+					</Tooltip>
+				</li>
+				<li className="text-center m-2">
+					<Tooltip
+						title="Portfólio"
+						placement="left"
+						TransitionComponent={Zoom}
+						arrow
+					>
+						<a href="#portfolio">
+							<AutoStoriesIcon color="primary" />
+						</a>
+					</Tooltip>
+				</li>
+				<li className="text-center m-2">
+					<Tooltip
+						title="Resumo"
+						placement="left"
+						TransitionComponent={Zoom}
+						arrow
+					>
+						<a href="#resume">
+							<WorkIcon color="primary" />
+						</a>
+					</Tooltip>
+				</li>
+				<li className="text-center m-2">
+					<Tooltip
+						title="Subir"
+						placement="left"
+						TransitionComponent={Zoom}
+						arrow
+					>
+						<a href="#" target="_top">
+							<ArrowCircleUpIcon color="primary" />
+						</a>
+					</Tooltip>
+				</li>
+				<li className="text-center m-2">
+					<Tooltip
+						title="Descer"
+						placement="left"
+						TransitionComponent={Zoom}
+						arrow
+					>
+						<a href="#" target="_top">
+							<ArrowCircleDownIcon color="primary" />
+						</a>
+					</Tooltip>
+				</li>
+			</ul>
+		</section>
+	)
+}
