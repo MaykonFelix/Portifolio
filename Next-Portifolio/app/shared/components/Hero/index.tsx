f97f4b974f149162a171c5ae1ff03c7dcbf582c8
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-08-04 21:18:45 -0300

    update portifolio

--- Next-Portifolio/app/shared/components/Hero/index.tsx
+++ Next-Portifolio/app/shared/components/Hero/index.tsx
@@ -1,18 +1,23 @@
 export const Hero = () => {
 	return (
-		<div className="flex border  border-red-500  ">
-			<div className="border border-blue-500 w-3/6 p-4">
-				<h1 className="text-2xl font-bold text-slate-500 s">Sobre</h1>
-				<h2 className="pb-2">Desenvolvedor Web</h2>
-				<p className="text-sm text-slate-500 font-medium">
+		<div className="flex">
+			<div className="  w-3/6 p-4">
+				<h1 className="text-2xl font-bold text-slate-600">Sobre</h1>
+				<h2 className="pb-2 text-slate-500">Desenvolvedor Web</h2>
+				<p className="text-sm text-slate-500 font-medium tracking-normal leading-loose">
 					Ola meu nome é <span className="text-rose-500 font-bold">Maykon</span>
 					, sou um desenvolvedor web com mais de 2 anos de experiencia, mas
 					sempre em busca de crescimento profissional na área de
 					Desenvolvimento, de forma a contribui com o crescimento empresarial
-					com medidas inovadoras e conhecimentos profissioanis, de
+					com medidas inovadoras e conhecimentos profissioanis, somar esforços
+					junto com a empresa que estou atuando.
 				</p>
+				<div className="flex border">
+					<div className="border">Teste</div>
+					<div className="border">Teste</div>
+				</div>
 			</div>
-			<div className="border border-green-500 w-3/6 p-4"></div>
+			<div className=" w-3/6 p-4"></div>
 		</div>
 	)
 }
