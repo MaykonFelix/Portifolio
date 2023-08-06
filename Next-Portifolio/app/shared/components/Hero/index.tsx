e17df67fae6f49735e2096f1f9d92b2432aadfd7
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-08-06 18:44:23 -0300

    new layout hero page

--- Next-Portifolio/app/shared/components/Hero/index.tsx
+++ Next-Portifolio/app/shared/components/Hero/index.tsx
@@ -1,9 +1,22 @@
 export const Hero = () => {
 	return (
-		<div className="flex">
-			<div className="  w-3/6 p-4">
-				<h1 className="text-2xl font-bold text-slate-600">Sobre</h1>
-				<h2 className="pb-2 text-slate-500">Desenvolvedor Web</h2>
+		<div className="flex ">
+			<div className="p-4 flex flex-col border">
+				<h3 className="text-xl font-bold text-black">Ola sou Maykon</h3>
+				<div className="flex ">
+					<h1 className="font-extrabold text-8xl text-black leading-[0.9]">
+						WEB
+					</h1>
+					<div className="flex items-end">
+						<div className="border w-10 h-10 bg-black" />
+						<div className="border w-10 h-10 bg-black" />
+						<div className="border w-10 h-10 bg-black" />
+					</div>
+				</div>
+				<h1 className="pb-2 font-extrabold text-8xl text-slate-500">
+					DEVELOPER
+				</h1>
+
 				<p className="text-sm text-slate-500 font-medium tracking-normal leading-loose">
 					Ola meu nome é <span className="text-rose-500 font-bold">Maykon</span>
 					, sou um desenvolvedor web com mais de 2 anos de experiencia, mas
@@ -12,12 +25,7 @@ export const Hero = () => {
 					com medidas inovadoras e conhecimentos profissioanis, somar esforços
 					junto com a empresa que estou atuando.
 				</p>
-				<div className="flex border">
-					<div className="border">Teste</div>
-					<div className="border">Teste</div>
-				</div>
 			</div>
-			<div className=" w-3/6 p-4"></div>
 		</div>
 	)
 }
