3945e3191287d15421809393a228795f9598a11f
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-08-08 20:05:11 -0300

    update

--- Next-Portifolio/app/shared/components/Hero/index.tsx
+++ Next-Portifolio/app/shared/components/Hero/index.tsx
@@ -7,27 +7,34 @@ import Tooltip from '@mui/material/Tooltip'
 
 export const Hero = () => {
 	return (
-		<div className="flex ">
-			<div className="p-4 flex flex-col border">
+		<div className="flex justify-center">
+			<div className="p-4 flex flex-col ">
 				<h3 className="text-xl font-bold text-black">Ola sou Maykon</h3>
 				<div className="flex ">
-					<h1 className="font-extrabold text-9xl text-black leading-[0.9]">
+					<h1 className="font-extrabold text-[14.5rem] text-black leading-[0.9]">
 						WEB
 					</h1>
 					<div className="flex items-end">
 						<div className="cursor-pointer">
 							<Tooltip title="GitHub">
-								<GitHubIcon />
+								<GitHubIcon sx={{ fontSize: 70 }} />
 							</Tooltip>
 						</div>
-						<LinkedInIcon />
-						<DescriptionIcon />
+						<div className="cursor-pointer">
+							<Tooltip title="Linkedin">
+								<LinkedInIcon sx={{ fontSize: 70 }} />
+							</Tooltip>
+						</div>
+						<div className="cursor-pointer">
+							<Tooltip title="Curriculo">
+								<DescriptionIcon sx={{ fontSize: 70 }} />
+							</Tooltip>
+						</div>
 					</div>
 				</div>
-				<h1 className="pb-2 pt-2 font-extrabold text-7xl text-slate-500">
+				<h1 className="pb-2 pt-2 font-extrabold text-9xl text-slate-500">
 					DEVELOPER
 				</h1>
-
 				<p className="text-xs text-slate-500 font-medium tracking-normal leading-loose border max-w-md">
 					Mais de 2 anos de experiencia em desenvolvimento, atualmente com 34
 					anos trabalhando em desenvolvimento front end.
