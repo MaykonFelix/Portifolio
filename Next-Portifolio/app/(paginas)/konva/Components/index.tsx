bf368642f70b0da87fcb67a827a7aa33e7e6f11a
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-08-23 00:13:28 -0300

    feat konva add

---
+++ Next-Portifolio/app/(paginas)/konva/Components/index.tsx
@@ -0,0 +1,14 @@
+import { Stage, Layer, Rect, Circle } from 'react-konva'
+
+const MapKonva = () => {
+	return (
+		<Stage draggable width={window.innerWidth} height={window.innerHeight}>
+			<Layer>
+				<Rect width={50} height={50} fill="red" />
+				<Circle x={200} y={200} stroke="black" radius={50} />
+			</Layer>
+		</Stage>
+	)
+}
+
+export default MapKonva
---
+++ Next-Portifolio/app/(paginas)/konva/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import dynamic from 'next/dynamic'
+
+const MapKonva = dynamic(() => import('./Components/index'), {
+	ssr: false,
+})
+
+const konva = () => {
+	return (
+		<div className='border -translate-y-[80px]'>
+			<MapKonva />
+		</div>
+	)
+}
+
+export default konva
