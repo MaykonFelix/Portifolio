2d178d7169f1207cce953702f84376c701eb6372
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-08-28 23:09:51 -0300

    update particle

--- Next-Portifolio/app/(paginas)/konva/page.tsx
+++ Next-Portifolio/app/(paginas)/konva/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import dynamic from 'next/dynamic'
 
 const MapKonva = dynamic(() => import('./Components/index'), {
--- Next-Portifolio/app/(paginas)/particle/page.tsx
+++ Next-Portifolio/app/(paginas)/particle/page.tsx
@@ -1,3 +1,33 @@
+'use client'
+
+import Particles from 'react-tsparticles'
+import { useCallback, useState } from 'react'
+import { loadSlim } from 'tsparticles-slim'
+import type { Container, Engine } from 'tsparticles-engine'
+import amongUs from './Options/amongUs'
+
 export default function particle() {
-	return <div>Particle</div>
+	const particlesInit = useCallback(async (engine: Engine) => {
+		await loadSlim(engine)
+	}, [])
+
+	const particlesLoaded = useCallback(
+		async (container: Container | undefined) => {
+			await console.log(container)
+		},
+		[],
+	)
+
+	const [option, setOption] = useState(amongUs)
+
+	return (
+		<div className="bg-slate-500">
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={option}
+			/>
+		</div>
+	)
 }
